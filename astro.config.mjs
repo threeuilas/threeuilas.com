42bc06345a634d088a97578fa8fbfec280be8678
Kailani Chu <me@kailani.io>
2025-12-08 00:10:53 -1000

    Add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://threeuilas.com', // Update this to your GitHub Pages URL if different
   vite: {
     plugins: [tailwindcss()]
   }
