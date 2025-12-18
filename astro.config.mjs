2e34a40901556b5b1c03dd3a6f1bdcfdd28ab2ef
Kailani Chu <me@kailani.io>
2025-12-18 11:10:29 -1000

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://threeuilas.com', // Update this to your GitHub Pages URL if different
+  // Update this to your GitHub Pages URL if different
+  site: 'https://threeuilas.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
